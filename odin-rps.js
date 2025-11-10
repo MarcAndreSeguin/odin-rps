09163d68bbfacaf042e834a7dcf31963fc4a96ef
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-11-09 21:10:51 -0500

    Added the playGame() function. Move the playRound() function inside the playGame() as well as global variables. Modified the index.html to make the game playable by user in the browser with results displayed in the console.

--- odin-rps.js
+++ odin-rps.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
   // let computerChoice;
   switch (randomNumber) {
     case 1:
-        return 'rock';
+      return "rock";
     case 2:
-        return 'paper';
+      return "paper";
     case 3:
-        return 'scissors';
+      return "scissors";
   }
 }
 
@@ -15,33 +15,70 @@ function getHumanChoice() {
   const RPS = prompt("Rock, paper, scissors?", "rock");
   const RPSLowerCase = RPS.toLowerCase();
   switch (RPSLowerCase) {
-    case 'rock':
-        return 'rock';
-    case 'paper':
-        return 'paper';
-    case 'scissors':
-        return 'scissors';
+    case "rock":
+      return "rock";
+    case "paper":
+      return "paper";
+    case "scissors":
+      return "scissors";
     default:
-        return 'rock';
+      return "rock";
   }
 }
 
-let humanScore = 0
-let computerScore = 0
+// let humanScore = 0
+// let computerScore = 0
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice == computerChoice) {
-    console.log("It's a tie because " + humanChoice + " and " + computerChoice + " is a stalemate.")
-  } else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
-    console.log("You win because " + humanChoice + " beats " + computerChoice + ".")
-    humanScore++;
-  } else {
-    console.log("You lose because " + computerChoice + " beats " + humanChoice + ".")
-    computerScore++;
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
+
+function playGame() {
+  // 5 rounds
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+      console.log(
+        "It's a tie because " +
+          humanChoice +
+          " and " +
+          computerChoice +
+          " is a stalemate."
+      );
+      return 'T';
+    } else if (
+      (humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+      console.log(
+        "You win because " + humanChoice + " beats " + computerChoice + "."
+      );
+      return 'H';
+    } else {
+      console.log(
+        "You lose because " + computerChoice + " beats " + humanChoice + "."
+      );
+      return 'C';
+    }
   }
+
+  for (let i = 0; i < 5; i++){
+        let currentRoundResult;
+        console.log("Round ", i+1)
+        currentRoundResult = playRound(getHumanChoice(),getComputerChoice())
+        if (currentRoundResult == 'C'){
+            computerScore++
+        } else if (currentRoundResult == 'H') {
+            humanScore++
+        }
+        console.log("TALLY -- Human:", humanScore, "Computer:", computerScore)
+        console.log("---")
+    }  
+
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+// playGame();
